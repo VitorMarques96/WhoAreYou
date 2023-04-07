43a657758f15e6b65e74dc66c18dd2c557691516
Vitor Marques <89469589+VitorMarques96@users.noreply.github.com>
2023-04-07 19:36:32 -0300

    Add files via upload

---
+++ Quem_e_Voce.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+profissao = input('Qual é a sua profissão?')
+
+print(f'{nome} tem {idade} anos e trabalha como {profissao}')
